77ee9a1b0a89b43625a739b9c64e1e5f893765f0
PongthepGeo <pongtep_tong@hotmail.com>
2022-12-14 14:24:49 +0700

    Day 3

--- test_outliers.py
+++ test_outliers.py
@@ -13,6 +13,14 @@ dummy = xxx.drop(drop_cols, axis=1)
 log_names     = ['GR', 'ILD_log10', 'DeltaPHI', 'PHIND', 'PE']
 max_out = [0.995, 0.995, 0.995, 0.995, 0.995] # ordering by wirelines
 for count, item in enumerate(log_names):
-    done_data = remove_outliers(dummy, item, 0., max_out[count])
+    y = remove_outliers(dummy, item, 0., max_out[count])
+max_out = [0.9, 0.9, 0.9, 0.9, 0.9] # ordering by wirelines
+for count, item in enumerate(log_names):
+    yy= remove_outliers(dummy, item, 0., max_out[count])
 # print(done_data)
+
+import matplotlib.pyplot as plt
+plt.scatter(y.GR, y.ILD_log10, s=30, edgecolors='None', alpha=1, cmap='red', marker='o')
+plt.scatter(yy.GR, yy.ILD_log10, s=30, edgecolors='None', alpha=0.5, cmap='black', marker='*')
+plt.show()
 
